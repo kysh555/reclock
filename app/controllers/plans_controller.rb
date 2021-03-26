f5a121220e197561914e2676fedbc725f15080e9
kysh555 <on88.sawada.ai@gmail.com>
2021-03-26 12:39:18 +0900

    add: posting plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,12 @@
 class PlansController < ApplicationController
+
+  def index
+    @plans = Plan.all
+  end
+
+  def create
+    plan = Plan.create(todo: params[:todo])
+    redirect_to root_path
+  end
+
 end
