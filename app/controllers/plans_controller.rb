423307d5824647ac35708675c25d837241855a70
kysh555 <on88.sawada.ai@gmail.com>
2021-03-26 12:52:35 +0900

    add: done function (server side)

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -5,8 +5,20 @@ class PlansController < ApplicationController
   end
 
   def create
-    plan = Plan.create(todo: params[:todo])
+    plan = Plan.create(todo: params[:todo], done: false)
     redirect_to root_path
+  end
+
+  def done
+    plan = Plan.find(params[:id])
+    if plan.done
+      plan.update(done: false)
+    else
+      plan.update(done: true)
+    end
+
+    item = Plan.find(params[:id])
+    render json:{plan: item}
   end
 
 end
