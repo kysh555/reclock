b0a6ce7dd90e1c11366bcbcd30bafa6619ac602e
kysh555 <on88.sawada.ai@gmail.com>
2021-03-26 12:38:57 +0900

    remove  column :done

--- db/migrate/20210323025806_create_plans.rb
+++ db/migrate/20210323025806_create_plans.rb
@@ -2,7 +2,6 @@ class CreatePlans < ActiveRecord::Migration[6.0]
   def change
     create_table :plans do |t|
       t.string  :todo, null: false
-      t.boolean :done
 
       t.timestamps
     end
