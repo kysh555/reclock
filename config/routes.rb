941fa27ae3601c59f6ce5585ccf0138dd5eb842e
kysh555 <on88.sawada.ai@gmail.com>
2021-03-30 13:05:23 +0900

    add: routing (delete list)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'plans#index'
-  resources :plans, only: [:create]
+  resources :plans, only: [:create, :destroy]
   get 'plans/:id', to: 'plans#done' 
 end
