996b63030f889739cd8da1a759bdfe463661b70d
kysh555 <on88.sawada.ai@gmail.com>
2021-03-30 13:21:38 +0900

    Revert "Feature todo list"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'plans#index'
-  resources :plans, only: [:create, :destroy]
-  get 'plans/:id', to: 'plans#done' 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
